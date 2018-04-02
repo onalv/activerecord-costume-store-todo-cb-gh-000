8f92cf0d3c04025e395eeccb3e119c58241d7998
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 16:14:45 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,11 +5,11 @@ class HauntedHouses << ActiveRecord::Migration
       t.string  :name
       t.string  :location
       t.string  :theme
-      t.integer :price
-      t.integer :friendly
+      t.float :price
+      t.boolean :friendly
       t.datetime  :opening_date
       t.datetime  :closing_date
-      t.string  :long_description
+      t.text  :long_description
     end
   end
 end
